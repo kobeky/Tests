b0397a9eb2669801495f9aba6ef6a30063aeb251
anzhuo <362390701@qq.com>
2016-09-09 17:07:32 +0800

    测试

--- app/src/main/java/com/example/anzhuo/tests/MainActivity.java
+++ app/src/main/java/com/example/anzhuo/tests/MainActivity.java
@@ -2,12 +2,56 @@ package com.example.anzhuo.tests;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+    public AMapLocationClient mLocationClient = null;
+    public AMapLocationClientOption mLocationOption = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mLocationClient=new AMapLocationClient(getApplicationContext());
+        mLocationOption=new AMapLocationClientOption();
+        mLocationClient.setLocationListener(mLocationListener);
+        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+        mLocationOption.setOnceLocationLatest(true);
+        mLocationOption.setNeedAddress(true);
+        mLocationOption.setWifiActiveScan(false);
+        mLocationOption.setMockEnable(false);
+        AMapLocationClientOption.setLocationProtocol(AMapLocationClientOption.AMapLocationProtocol.HTTP);
+        mLocationClient.setLocationOption(mLocationOption);
+        mLocationClient.startLocation();
+
     }
+    public AMapLocationListener mLocationListener = new AMapLocationListener() {
+        @Override
+        public void onLocationChanged(AMapLocation aMapLocation) {
+            if (aMapLocation != null) {
+                if (aMapLocation.getErrorCode() == 0) {
+                   String city= aMapLocation.getCity();
+                    SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                    Date date = new Date(aMapLocation.getTime());
+                    String time=df.format(date);
+                    Toast.makeText(MainActivity.this,city+""+time,Toast.LENGTH_SHORT).show();
+//可在其中解析amapLocation获取相应内容。
+                }else {
+                    //定位失败时，可通过ErrCode（错误码）信息来确定失败的原因，errInfo是错误信息，详见错误码表。
+                    Log.i("AmapError","location Error, ErrCode:"
+                            + aMapLocation.getErrorCode() + ", errInfo:"
+                            + aMapLocation.getErrorInfo());
+                }
+            }
+        }
+    };
 }
